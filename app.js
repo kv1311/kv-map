5acb8e95e40058e473b250cfc959b47973e1569e
Dhanush G <chanush02@gmail.com>
2023-11-16 19:13:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import { hospitals } from "./hospitals.js";
+var coords;
+var map;
+var marker;
+var startCoords;
+
+navigator.geolocation.getCurrentPosition((position) => {
+  coords = [position.coords.latitude, position.coords.longitude];
+
+  map = L.map("map").setView(coords, 7);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const hospitalMarkers = hospitals.map((hospital, index) => {
+    const latlng = hospital.latlng.split(",");
+    const marker = L.marker(latlng).addTo(map);
+    const circle = L.circle(latlng, 500, {
+      color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+    }).addTo(map);
+    marker.bindPopup(hospital.hospital).openPopup();
+  });
+  function calculateDistances(startCoords, endCoords) {
+    const waypoints = [
+      L.latLng(startCoords.lat, startCoords.lng),
+      L.latLng(endCoords.split(",")[0], endCoords.split(",")[1]),
+    ];
+    L.Routing.control({ waypoints }).addTo(map);
+  }
+
+  function onMapClick(e) {
+    if (confirm("Confirm this location?")) {
+      startCoords = e.latlng;
+      hospitals.forEach((hospital) => {
+        calculateDistances(startCoords, hospital.latlng);
+      });
+    }
+  }
+  map.on("click", onMapClick);
+});
---
+++ hospitals.js
@@ -0,0 +1,47 @@
+export const hospitals = [
+  {
+    hospital: "Naagai Hospital",
+    pincode: 611106,
+    latlng: "10.7570, 79.8259",
+  },
+  {
+    hospital: "Sri Venkateswara Hospital",
+    pincode: 611001,
+    latlng: "10.7607, 79.8426",
+  },
+  {
+    hospital: "Govt Hospital",
+    pincode: 611002,
+    latlng: "10.578299, 79.753654",
+  },
+  {
+    hospital: "Pentagon hospital",
+    pincode: 611003,
+    latlng: "10.7640, 79.8408",
+  },
+  {
+    hospital: "Ansari Hospital",
+    pincode: 611001,
+    latlng: "10.7644, 79.8450",
+  },
+  {
+    hospital: "Srivaris Hospital",
+    pincode: 611003,
+    latlng: "10.76242815591584, 79.8399179314689",
+  },
+  {
+    hospital: "NAGAI HEAD HOSPITAL",
+    pincode: 611001,
+    latlng: "10.7732, 79.8439",
+  },
+  {
+    hospital: "Dr.Chandrasekar Hospital",
+    pincode: 611001,
+    latlng: "10.776539947794973, 79.8381846481993",
+  },
+  {
+    hospital: "Government medical college and hospital",
+    pincode: 611108,
+    latlng: "10.7181, 79.8051",
+  },
+];
