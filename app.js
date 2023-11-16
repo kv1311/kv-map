14e4387bc13066c44f913a3be4c8352afbf7a034
Dhanush G <chanush02@gmail.com>
2023-11-16 20:36:09 +0530

    find distance using custom function

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ var coords;
 var map;
 var startCoords;
 
+const GRAPHHOPPER_API_KEY = "8dda6af4-c857-46de-b071-602ea2fe8004";
+
+const getRouteInfo = async (start, end) => {
+  console.log([start, end]);
+  const query = new URLSearchParams({
+    instructions: true,
+    type: "json",
+    key: GRAPHHOPPER_API_KEY,
+  });
+  query.append("point", start);
+  query.append("point", end);
+  const response = await fetch(`https://graphhopper.com/api/1/route?${query.toString()}`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  console.log(data);
+};
+
 navigator.geolocation.getCurrentPosition((position) => {
   coords = [position.coords.latitude, position.coords.longitude];
 
@@ -13,7 +31,7 @@ navigator.geolocation.getCurrentPosition((position) => {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  hospitals.map((hospital, index) => {
+  hospitals.map((hospital) => {
     const latlng = hospital.latlng.split(",");
     const marker = L.marker(latlng).addTo(map);
     L.circle(latlng, 500, {
@@ -27,14 +45,24 @@ navigator.geolocation.getCurrentPosition((position) => {
       L.latLng(startCoords.lat, startCoords.lng),
       L.latLng(endCoords.split(",")[0], endCoords.split(",")[1]),
     ];
-    L.Routing.control({ waypoints }).addTo(map);
+    L.Routing.control({
+      waypoints,
+      router: L.Routing.graphHopper(GRAPHHOPPER_API_KEY, {
+        details: ["distance"],
+      }),
+    }).addTo(map);
   }
 
   function onMapClick(e) {
     if (confirm("Confirm this location?")) {
       startCoords = e.latlng;
       hospitals.forEach((hospital) => {
-        calculateDistances(startCoords, hospital.latlng);
+        const start = [startCoords.lat, startCoords.lng];
+        const end = [
+          parseFloat(hospital.latlng.replace(" ", "").split(",")[0]),
+          parseFloat(hospital.latlng.replace(" ", "").split(",")[1]),
+        ];
+        getRouteInfo(start, end);
       });
     }
   }
